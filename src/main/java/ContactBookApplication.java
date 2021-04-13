fd5c5cbb5a935f13f7bc45f550f385483c31bec7
Denis Dorofeev <buhhu8@yandex.ru>
2021-04-13 21:34:29 +0300

    sdf

---
+++ src/main/java/ContactBookApplication.java
@@ -0,0 +1,8 @@
+import org.springframework.boot.SpringApplication;
+
+public class ContactBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactBookApplication.class, args);
+    }
+}
---
+++ src/main/java/model/dto/UsersDto.java
@@ -0,0 +1,11 @@
+package model.dto;
+
+import lombok.Data;
+
+@Data
+public class UsersDto {
+
+    Integer userId;
+
+
+}
